1bda188fa246dcfd41d9973d1e1b9be988e24f73
Laurent Gautier <lgautier@gmail.com>
2022-09-17 15:48:49 -0400

    Fix linting.

--- rpy2_arrow/pyarrow_rarrow.py
+++ rpy2_arrow/pyarrow_rarrow.py
@@ -4,4 +4,4 @@ warnings.warn(
     'The module rpy2_arrow.pyarrow_rarrow is moved to rpy2_arrow.arrow',
     DeprecationWarning
 )
-from .pyarrow_rarrow import *
+from .pyarrow_rarrow import *  # noqa: E402,F401,F403
